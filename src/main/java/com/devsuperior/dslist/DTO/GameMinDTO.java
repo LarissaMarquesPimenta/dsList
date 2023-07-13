6bf52f8983952a1e4dc454fa445b1a42291a8dc8
LarissaMarquesPimenta <laariipimenta@gmail.com>
2023-07-13 12:19:48 -0300

    Service, repository e controller - GameMinDTO

---
+++ src/main/java/com/devsuperior/dslist/DTO/GameMinDTO.java
@@ -0,0 +1,69 @@
+package com.devsuperior.dslist.DTO;
+
+import com.devsuperior.dslist.entities.Game;
+
+import jakarta.persistence.Column;
+
+	public class GameMinDTO {
+	
+		private Long id;
+		private String title;
+		private Integer year;
+		private String imgUrl;
+		private String shortDescription;
+	
+	
+	public GameMinDTO() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public GameMinDTO(Game entity) {
+		super();
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.year = entity.getYear();
+		this.imgUrl = entity.getImgUrl();
+		this.shortDescription = entity.getShortDescription();
+	}
+	
+}
